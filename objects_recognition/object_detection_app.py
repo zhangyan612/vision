b0bc5c5cd67885957f79c8a2c8c3e8501d62f373
Yan Zhang <zhangyan612@gmail.com>
2017-10-07 13:12:01 -0500

    add object recognition only using cv2

---
+++ objects_recognition/object_detection_app.py
@@ -0,0 +1,5 @@
+# import tensorflow
+
+# tensorflow only work on 64 bit, need python 3
+# https://medium.com/towards-data-science/building-a-real-time-object-recognition-app-with-tensorflow-and-opencv-b7a2b4ebdc32
+# https://github.com/datitran/object_detector_app
