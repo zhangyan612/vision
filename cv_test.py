5bd9e0e009fe8632bbeb214e60e3d1d60266b0d1
Yan Zhang <zhangyan612@gmail.com>
2017-10-07 16:07:36 -0500

    detector running

--- cv_test.py
+++ cv_test.py
@@ -1,8 +1,9 @@
 import cv2
-print(cv2.__version__)
+# print(cv2.__version__)
 # import sys
 # print(sys.path)
 # sift = cv2.xfeatures2d.SIFT_create()
 # detector = cv2.SIFT()
 
-help(cv2.face.LBPHFaceRecognizer_create())
+# help(cv2.face.LBPHFaceRecognizer_create())
+help(cv2)
--- recognition_camera/detector.py
+++ recognition_camera/detector.py
@@ -4,6 +4,11 @@ import numpy as np
 from PIL import Image 
 import pickle
 
+
+def draw_text(img, text, x, y):
+    cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
+
+
 recognizer = cv2.face.LBPHFaceRecognizer_create() # createLBPHFaceRecognizer()
 recognizer.read('trainer/trainer.yml')
 
@@ -12,7 +17,7 @@ faceCascade = cv2.CascadeClassifier(cascadePath)
 path = 'dataSet'
 
 cam = cv2.VideoCapture(0)
-# font = cv2.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1) #Creates a font
+#font = cv2.InitFont(cv2.CV_FONT_HERSHEY_SIMPLEX, 1, 1, 0, 1, 1) #Creates a font
 while True:
     ret, im = cam.read()
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -21,12 +26,15 @@ while True:
         nbr_predicted, conf = recognizer.predict(gray[y:y+h, x:x+w])
         cv2.rectangle(im, (x-50, y-50), (x+w+50, y+h+50), (225, 0, 0), 2)
         print(nbr_predicted)
-        # if(nbr_predicted==7):
-        #      nbr_predicted='Obama'
-        # elif(nbr_predicted==2):
-        #      nbr_predicted='Anirban'
+        if nbr_predicted == 1:
+             nbr_predicted = 'Yan Zhang'
+        elif nbr_predicted == 2:
+             nbr_predicted='Diana Barron'
         # cv2.putText(img, text, (x, y), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 0), 2)
-        # cv2.PutText(cv2.fromarray(im), str(nbr_predicted)+"--"+str(conf), (x, y+h), font, 255) # Draw the text
+        #cv2.PutText(cv2.fromarray(im), str(nbr_predicted)+"--"+str(conf), (x, y+h), font, 255) # Draw the text
+        draw_text(im, str(nbr_predicted)+"--"+str(conf), x, y+h)
+        #cv2.putText(im, str(nbr_predicted)+"--"+str(conf), (x, y+h), cv2.FONT_HERSHEY_PLAIN, 255)  # Draw the text
+
         cv2.imshow('im', im)
         cv2.waitKey(10)
 
